bf0a5163a658582080695ca0ea3f5e200f9470ff
RaulVictor-m <raulvictor822@gmail.com>
2024-06-29 19:07:02 -0300

    adding termbox lib

--- build.zig
+++ build.zig
@@ -24,6 +24,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.linkLibC();
+    exe.addIncludePath(b.path("dependencies"));
+    exe.addCSourceFile(.{
+        .file = b.path("src/termbox.c"),
+        .flags = &.{}});
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
---
+++ src/termbox.c
@@ -0,0 +1,4 @@
+#define TB_IMPL
+#include "../dependencies/termbox.h"
+
+// this files only serves to hold the implementation of termbox to be linked
